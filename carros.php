e0ecb0eac5dbc7559b23732e5b2aa5a771438e53
Paulo <paulopchjr@gmail.com>
2021-01-22 10:15:17 -0300

    ok

---
+++ carros.php
@@ -0,0 +1,67 @@
+<?php 
+
+
+
+$veiculos = array(
+	array(
+		'marca'=>'marcas',
+		'modelo'=>array(
+
+			// INICIO: VOLKS
+			array(
+				'marca'=>'VOLKSWAGEM',
+				'modelo'=> array(
+					// INICIO DO JETTA
+					array(
+						'marca'=>"JETTA",
+						'cor'=>"Branco"
+					)//FIM DO JETA
+				)
+			),
+			// FIM : VOLKS
+
+			//INICIO:TOYOTA
+			array(
+				'marca'=>'TOYOTA',
+				'modelo'=>array(
+					// INICIO DO COROLA
+					array(
+						'marca'=>'COROLA',
+						'cor'=>'Prata'
+					), //  FIM COROLA
+				)
+			)
+		)
+	)
+);
+
+
+	function exibe($carros){
+
+
+		$html ='<ul>';
+
+		foreach ($carros as $car) {
+			$html.='<li>';
+			$html.=$car['marca'];
+
+			if (isset($car['modelo']) && count($car['modelo']) > 0){
+				$html.= exibe($car['modelo']);
+			}
+			$html.='</li>';
+		}
+
+	   $html.='</ul>';
+
+	   return $html;
+
+	}
+
+
+	echo exibe($veiculos);
+
+
+
+
+
+?>
